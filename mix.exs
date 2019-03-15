90dd7aa47b343e13c664274920787543ef1ad75c
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2019-03-15 17:42:25 +0800

    Add preliminary dependencies

--- mix.exs
+++ mix.exs
@@ -5,9 +5,23 @@ defmodule FluminusBot.MixProject do
     [
       app: :fluminus_bot,
       version: "0.1.0",
-      elixir: "~> 1.8",
+      elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      source_url: "https://github.com/indocomsoft/fluminus_bot",
+      docs: [
+        main: "readme",
+        extras: ["REAMDE.md"]
+      ],
+      package: package(),
+      description: description(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -21,8 +35,22 @@ defmodule FluminusBot.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:fluminus, "~> 0.2.3"},
+      {:dialyxir, "~> 1.0.0-rc.4", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.10", only: :test},
+      {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  defp description do
+    "A Telegram Bot that does push notification for LumiNUS announcements (https://luminus.nus.edu.sg)"
+  end
+
+  defp package() do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/indocomsoft/fluminus_bot"}
     ]
   end
 end
